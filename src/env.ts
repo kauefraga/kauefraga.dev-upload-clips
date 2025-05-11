0f5a90313cbab570a154d65825f711a931208926
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-05-11 20:33:50 -0300

    Update .env.example and reorder

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config';
 import { z } from 'zod';
 
 const EnvSchema = z.object({
-	S3_BUCKET_NAME: z.string().default('kauefraga-dev'),
+	S3_BUCKET_NAME: z.string(),
 	S3_ACCESS_KEY: z.string(),
 	S3_SECRET_KEY: z.string(),
+	COOKIE_SECRET: z.string().min(8),
 });
 
 export const env = EnvSchema.parse(process.env);
