64928e6f0b966fd0a0477a56d7d527094c31b8dc
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-05-12 23:32:23 -0300

    Set S3_ENDPOINT environment variable

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,7 @@ const EnvSchema = z.object({
 	S3_BUCKET_NAME: z.string(),
 	S3_ACCESS_KEY: z.string(),
 	S3_SECRET_KEY: z.string(),
+	S3_ENDPOINT: z.string().url(),
 	COOKIE_SECRET: z.string().min(8),
 });
 
